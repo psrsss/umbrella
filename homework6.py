adef65ad0bbf2a31b784072b42fdbcc5265614b4
psrsss <artem.dmuh@icloud.com>
2026-03-11 02:31:22 +0300

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -1,3 +1,12 @@
+#Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
+#Напишите программу, которая добавляет ‘ing’ к словам
+# В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
+# Напишите программу которая удаляет пробел в начале, в конце строки
+# Имена собственные всегда начинаются с заглавной буквы, за которой следуют строчные буквы. Исправьте данное имя собственное так, чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris"
+# Перевести строку в список "Robin Singh" => ["Robin”, “Singh"], "I love arrays they are my favorite" => ["I", "love", "arrays", "they", "are", "my", "favorite"]
+# Дан список: [Robin Singh], и 2 строки: "Welcome" и "airport". Напечатайте текст: “Hello, Robin Singh! Welcome to airport”
+# Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"] сделайте из него строку => "I love arrays they are my favorite"
+# Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение, удалите элемент из списка под индексом 6
 s1 = 'www.my_site#about'
 print(s1.replace("#", "/"))
 s2 = 'This is a test message'
