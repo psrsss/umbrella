14eeae6d0d1148597d4471d0cbfd34957f7e2d55
Artem Dmukhouski <artem.dmuh@icloud.com>
2026-03-16 23:35:04 +0300

    add homework7

---
+++ levelUp.py
@@ -0,0 +1,5 @@
+# LevelUp
+EX = int(input("Enter your experience: "))
+TH = int(input("Enter threshold: "))
+RW = int(input("Enter reward: "))
+print(EX + RW >= TH)
---
+++ time.py
@@ -0,0 +1,12 @@
+# Time
+N = int(input("Enter timer time: "))
+HOURS = int(N/60)
+MINUTES = N % 60
+if N >= 0:
+    HH = f"{HOURS:02d}"
+    MM = f"{MINUTES:02d}"
+    print(HH, ":", MM)
+    DIGITS_SUM = sum(int(DIGIT) for DIGIT in (HH + MM))
+    print("Sum of digits: ", DIGITS_SUM)
+else:
+    print("Enter correct timer time")
