130251d6fb0d89491f668ec03b2ba962a4425ba5
psrsss <artem.dmuh@icloud.com>
2026-03-26 18:45:40 +0300

    Update Сandles.py

--- Сandles.py
+++ Сandles.py
@@ -1,3 +1,4 @@
+"""Candles"""
 def solution(candle_number, make_new):
     burnt = candle_number
     remainder = candle_number
@@ -10,7 +11,7 @@ def solution(candle_number, make_new):
 
 print(solution(15, 5))
 assert solution(5, 2) == 9
-assert solution(1, 2) == 1\
+assert solution(1, 2) == 1
 assert solution(15, 5) == 18
 assert solution(12, 2) == 23
 assert solution(6, 4) == 7
