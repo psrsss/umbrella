56ed4bb963ad029fc0153dab4d7e96b06ecee409
psrsss <artem.dmuh@icloud.com>
2026-03-31 19:07:59 +0300

    Update type_decorator.py

--- type_decorator.py
+++ type_decorator.py
@@ -12,26 +12,29 @@ def typed(type_):
 
 
 @typed(type_=str)
-def add(a, b):
+def add_str(a, b):
+    """Adds a and b"""
     print(a + b)
 
 
-add("3", 5)
-add(5, 5)
-add('a', 'b')
+add_str("3", 5)
+add_str(5, 5)
+add_str('a', 'b')
 
 
 @typed(type_=int)
-def add(a, b, с):
-    print(a + b + с)
+def add_int(a, b, c):
+    """Sums a, b and c"""
+    print(a + b + c)
 
 
-add(5, 6, 7)
+add_int(5, 6, 7)
 
 
 @typed(type_=float)
-def add(a, b, с):
-    print(a + b + с)
+def add_float(a, b, c):
+    """Sums a, b and c"""
+    print(a + b + c)
 
 
-add(0.1, 0.2, 0.4)
+add_float(0.1, 0.2, 0.4)
