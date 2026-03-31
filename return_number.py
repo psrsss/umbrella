191457e50be46c894a960adfd6d720da810f0cdb
psrsss <artem.dmuh@icloud.com>
2026-03-31 19:14:18 +0300

    Update return_number.py

--- return_number.py
+++ return_number.py
@@ -2,7 +2,9 @@
 
 
 def decorator(func):
+    """Args decorator"""
     def wrapper(*args, **kwargs):
+        """Function wrapper"""
         all_args = list(args) + list(kwargs.values())
         for arg in all_args:
             if isinstance(arg, (int, float)):
@@ -16,6 +18,7 @@ def decorator(func):
 
 @decorator
 def numbers(a, b):
+    """Prints a and b"""
     print(a, b)
 
 
